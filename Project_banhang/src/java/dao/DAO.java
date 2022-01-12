a4bd1328f13c5ffec8f21e6e7b87e2317511193c
NhuVanKun <nhudinhvanfpt1999@gmail.com>
2022-01-12 23:34:38 +0700

    first commit

---
+++ Project_banhang/src/java/dao/DAO.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import DAL.DBContext;
+import entity.Category;
+import entity.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author trinh
+ */
+public class DAO extends DBContext {
+
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct() {
+        List<Product> list = new ArrayList<>();
+        String sql = "select * from product";
+        try {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Category> getAllCategory() {
+        List<Category> list = new ArrayList<>();
+        String sql = "select * from Category";
+        try {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Category(rs.getInt(1),
+                        rs.getString(2)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public Product getProductById() {
+        String sql = "select top 1 * from product \n"
+                + "order by id desc";
+        try {
+            ps=connection.prepareStatement(sql);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        DAO dao = new DAO();
+        List<Category> list = dao.getAllCategory();
+        for (Category o : list) {
+            System.out.println(o);
+        }
+    }
+}
